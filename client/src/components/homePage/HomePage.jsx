0c9f69135a6d95cf44d4ad7cc17f01657e9239cb
FlorAccastelli <f.accastelli@gmail.com>
2024-03-04 12:32:54 -0300

    Adding basic stylesheet to home page

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import fakeCards from "../../fakeCards";
+import styles from './homePage.module.css'
   
   export default function HomePage() {
     const Cards = fakeCards.map(c=> 
-        <div key={c.id}>{c.name.forename}
-        <img src={c.image.url}></img>
-        <p>Teams: {c.teams}</p>
+        <div className={styles.card} key={c.id}>
+            <p>{c.name.forename}</p>
+            <img className={styles.cardImage} src={c.image.url}></img>
+            <p> <b>Teams:</b> {c.teams}</p>
         </div>)
 
     return(
