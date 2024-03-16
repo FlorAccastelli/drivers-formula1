e8851b77b6fde101c7bb8ccd15b53188863e55ff
FlorAccastelli <f.accastelli@gmail.com>
2024-03-16 19:33:48 -0300

    Validation form working

--- client/src/components/formPage/CreateDriver.jsx
+++ client/src/components/formPage/CreateDriver.jsx
@@ -3,6 +3,7 @@ import styles from "./createDriver.module.css"
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import validation from "./validation";
 
 export default function CreateDriver() {
     const [driverData, setDriverData] = useState({ 
@@ -17,7 +18,9 @@ export default function CreateDriver() {
     const [errors, setErrors] = useState({})
 
     const handleChange = (event) => {
+        setErrors(validation({...driverData, [event.target.name]:event.target.value }))
         setDriverData({...driverData, [event.target.name]:event.target.value })
+        
     }
 
     const handleSubmit = async (event) => {
@@ -43,37 +46,44 @@ export default function CreateDriver() {
     }
     
     return (
-        <div>
-            <h1>CREATE DRIVER</h1>
+        <div className={styles.background}>
             <form onSubmit={handleSubmit} className={styles.container}>
                 <label>
                     Name:
-                    <input type="text" placeholder="Ingrese un nombre" id="name" name="name" value={driverData.name} onChange={handleChange}/>
+                    <input type="text" placeholder="Ingrese un nombre" id="name" name="name" value={driverData.name} onChange={handleChange} className={ errors.name && styles.warning }/>
+                    
                 </label>
+                { errors.name && <p className={styles.danger}>{errors.name}</p> } 
                 <br />
                 <label>
                     Surname:
-                    <input type="text" placeholder="Ingrese un apellido" id="surname" name="surname" value={driverData.surname} onChange={handleChange}/>
+                    <input type="text" placeholder="Ingrese un apellido" id="surname" name="surname" value={driverData.surname} onChange={handleChange} className={ errors.surname && styles.warning }/>
+                    
                 </label>
+                { errors.surname && <p className={styles.danger}>{errors.surname}</p> } 
                 <br />
                 <label>
                     Nationality:
-                    <input type="text" placeholder="Ingrese un país" id="nationality" name="nationality" value={driverData.nationality} onChange={handleChange}/>
+                    <input type="text" placeholder="Ingrese un país" id="nationality" name="nationality" value={driverData.nationality} onChange={handleChange} className={ errors.nationality && styles.warning }/>
                 </label>
+                { errors.nationality && <p className={styles.danger}>{errors.nationality}</p> } 
                 <br />
                 <label>
                     Image:
-                    <input type="url" placeholder="Ingrese la URL de la imagen" id="image" name="image" value={driverData.image} onChange={handleChange}/>
+                    <input type="url" placeholder="Ingrese la URL de la imagen" id="image" name="image" value={driverData.image} onChange={handleChange} className={ errors.image && styles.warning }/>
                 </label>
+                { errors.image && <p className={styles.danger}>{errors.image}</p> } 
                 <br />
                 <label>
                     Dob:
-                    <input type="date" placeholder="Fecha de nacimiento" id="dob" name="dob" value={driverData.dob} onChange={handleChange}/>
+                    <input type="date" placeholder="Fecha de nacimiento" id="dob" name="dob" value={driverData.dob} onChange={handleChange} className={ errors.dob && styles.warning }/>
                 </label>
+                { errors.dob && <p className={styles.danger}>{errors.dob}</p> } 
                 <br />
                 <label>
-                    <textarea name="description" rows="7" cols="30" value={driverData.description} onChange={handleChange}>Describa a su driver</textarea>
+                    <textarea name="description" rows="6" cols="20" value={driverData.description} onChange={handleChange} className={ errors.description && styles.warning } placeholder="Describa a su driver"></textarea>
                 </label>
+                { errors.description && <p className={styles.danger}>{errors.description}</p> } 
                 <br />
                 <label>
                     Teams asociados:
---
+++ client/src/components/formPage/validation.js
@@ -0,0 +1,29 @@
+const charsRegex = /^[a-zA-Z\s]+$/
+const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+function validation (data) {
+    const errors = {};
+    if(!charsRegex.test(data.name)) errors.name = "No se permiten caracteres especiales en el nombre";
+    if(!data.name) errors.name = "Campo requerido";
+    if(data.surname.length > 30) errors.surname = "La longitud debe ser de 30 caracteres como máximo";
+    if(!data.surname) errors.surname = "Campo requerido";
+    if(!data.nationality) errors.nationality = "Se requiere asignar un país";
+    if(urlRegex.test(data.image)) errors.image = "Asignar URL correctamente";
+    if(!data.image) errors.image = "Campo requerido";
+    if (!data.dob) errors.dob = "Se requiere ingresar la fecha de nacimiento";
+    else {
+        const dobDate = new Date(data.dob);
+        const minDate = new Date();
+        minDate.setFullYear(minDate.getFullYear() - 13);
+        if (dobDate > minDate) {
+            errors.dob = "El conductor no puede ser menor a 13 años";
+        }
+    }
+    if(data.description.length < 3 || data.description.length > 200) errors.description = "Se permiten entre 3 y 200 caracteres";
+    if(!data.description) errors.description = "Campo requerido";
+    
+
+    return errors;
+}
+
+export default validation;
